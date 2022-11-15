ae85405b598c1484b7c0a70c6f022ccbb3fa57a5
vrmcardoso <vrmcardoso@gmail.com>
2022-11-15 12:52:37 +0000

    task manager coiso

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new"
 end
